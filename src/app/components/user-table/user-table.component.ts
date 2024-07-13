11219c33c4f646300a64dbe317fa8d20ffd2da9e
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-07-13 16:56:24 +0530

    pagination

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, ViewChild, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  OnChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '.././../services/user.service';
@@ -6,22 +13,26 @@ import { UserService } from '.././../services/user.service';
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.css']
+  styleUrls: ['./user-table.component.css'],
 })
 export class UserTableComponent implements OnInit, OnChanges {
   displayedColumns: string[] = ['name', 'workoutType', 'minutes'];
   users = new MatTableDataSource<any>();
   totalUsers = 0;
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
 
   @Input() searchTerm: string = '';
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.loadUsers();
-    this.users.paginator = this.paginator;
+    this.cdr.detectChanges();
+    this.users.paginator = this.matPaginator;
   }
 
   ngOnChanges() {
